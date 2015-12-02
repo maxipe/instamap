4d4a979550505a3773a258540eb9284627107e69
maxipe <maxipenia@hotmail.com>
2015-12-02 14:45:33 +0100

    Added template with map.

--- city/views.py
+++ city/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django import forms
+from django.http import JsonResponse
 
-# Create your views here.
+from models import Coordinate
+from django.views.decorators.csrf import csrf_exempt #fixme Workaround
+
+import requests
+
+@csrf_exempt
+def index(request):
+    form = forms.CharField(label='City', max_length=100)
+    
+    #return HttpResponse("Hello world! -")
+    return render(request, 'index.html', {'form':form})
+
+#fixme: workaround
+@csrf_exempt
+def getCityInfo(request):
+
+    if not request.POST["city-name"]:
+            return redirect('/')
+
+    cityName = request.POST["city-name"]
+    r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+cityName)
+    cityJson = r.json()
+
+    #print cityJson["results"][0]["geometry"]["location"]
+
+    coordinate = Coordinate()
+    coordinate.latitude = cityJson["results"][0]["geometry"]["location"]["lat"]
+    coordinate.longitude = cityJson["results"][0]["geometry"]["location"]["lng"]
+
+    #print request.POST
+    #print coordinate.latitude
+    return render(request, 'index.html', {'coordinate':coordinate, 'cityName':cityName})
