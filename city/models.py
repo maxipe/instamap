9de8bbbde8b8480b93b6cbd152efdee8c27fbf60
maxipe <maxipenia@hotmail.com>
2016-01-02 19:19:06 +0100

    Display of photos on map.

--- city/models.py
+++ city/models.py
@@ -4,6 +4,13 @@ class Coordinate(models.Model):
     latitude = models.CharField(max_length=20)
     longitude = models.CharField(max_length=20)
 
+    def distance(self, other):
+        latitudeDifference = abs(self.latitude - other.latitude)
+        longitudeDifference = abs(self.longitude - other.longitude)
+
+        #Without root... Not "real distance".
+        return latitudeDifference**2 + longitudeDifference**2
+
     def __str__(self):
         return "("+self.latitude+" , "+self.longitude+")"
 
@@ -20,19 +27,101 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+class Block(models.Model):
+    lowerLeft = models.ForeignKey(Coordinate, related_name='LL')
+    upperRight = models.ForeignKey(Coordinate, related_name='UR')
+
+    def contains(self, c):
+        '''C is Coordinate. '''
+        return \
+            c.latitude > lowerLeft.latitude and \
+            c.latitude < upperRight.latitude and \
+            c.longitude > lowerLeft.longitude and \
+            c.longitude < upperRight.longitude
+
+"""
+class Block(object):
+
+    def contains(self, c):
+        '''C is Point. '''
+        return \
+            c.x >= lowerLeft.x and \
+            c.x < upperRight.x and \
+            c.y >= lowerLeft.y and \
+            c.y < upperRight.y
+
+    def divideInFour(self):
+        leftX = lowerLeft.x
+        rightX = upperLeft.x
+        middleX = (rightX + leftX) / 2.0
+
+        bottomY = lowerLeft.y
+        topY = upperRight.y
+        middleY = (topY + bottomY) / 2.0
+
+        middlePoint = Point(middleX, middleY)
+
+        topLeft = Block( Point(leftX,middleY), Point(middleX,topY) )
+        topRight = Block( middlePoint, self.upperRight )
+        bottomLeft = Block( self.lowerLeft, middlePoint )
+        bottomRight = Block( Point(middleX,bottomY), Point(rightX,middleY) )
+
+        return topLeft, topRight, bottomLeft, bottomRight
+
+class QuadTree(object):
+    NODE_CAPACITY = 10
+
+    @classmethod
+    #http://stackoverflow.com/questions/843580/writing-a-init-function-to-be-used-in-django-model
+    def create(self, block):
+
+        self.block = block
+        self.points = []
+
+        self.northWest = None
+        self.northEast = None
+        self.southWest = None
+        self.southEast = None
+
+    def contains(self, point):
+        return self.block.contains(point)
+
+    def subdivide(self):
+        self.northWest, self.northEast, self.southWest, self.southEast = self.block.divideInFour()
+
+    def insert(self, point):
+
+        if len(points) < NODE_CAPACITY:
+            self.points.append(point)
+            return True
+
+        if not self.northWest:
+            self.subdivide()
+
+        if self.insert(self.northWest) return True
+        if self.insert(self.northEast) return True
+        if self.insert(self.southWest) return True
+        if self.insert(self.southEast) return True
+
+        raise Exception("QuadTree didn't find a subtree...")
+"""
+
 class Photo(models.Model):
-    date = models.DateTimeField('date of point')
+    #date = models.DateTimeField('date of point')
     coordinate = models.ForeignKey(Coordinate)
-    likes = models.IntegerField(default=0)
-    coments = models.IntegerField(default=0)
+    #likes = models.IntegerField(default=0)
+    #coments = models.IntegerField(default=0)
     link = models.CharField(max_length=200)
 
     #Belongs to
-    city = models.ForeignKey(City)
-    author = models.ForeignKey(InstagramUser)
+    #city = models.ForeignKey(City)
+    #author = models.ForeignKey(InstagramUser)
 
     def __str__(self):
         return "In "+str(self.city)+" by "+str(self.author)+" at "+str(self.date)+"."
+
+    def distance(self, other):
+        return self.coordinate.distance(other.coordinate)
 
 class InterestingArea(models.Model):
     coordinate = models.ForeignKey(Coordinate)
--- city/views.py
+++ city/views.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse
 from django import forms
 from django.http import JsonResponse
 
-from models import Coordinate
+from models import Coordinate, Photo
 from django.views.decorators.csrf import csrf_exempt #fixme Workaround
 
 import requests
+
+import time
+import threading
+
+from quadtree import QuadTree, Block, Point
 
 @csrf_exempt
 def index(request):
@@ -16,16 +21,74 @@ def index(request):
     #return HttpResponse("Hello world! -")
     return render(request, 'index.html', {'form':form})
 
+def makeRequest(photos, coordinate, startTimestamp, finishTimestamp):
+
+    photosRequest = requests.get('https://api.instagram.com/v1/media/search?lat='+str(coordinate.latitude)+'&lng='+str(coordinate.longitude)+'&client_id=9e52d8b3795446fd82fe701eebe681f9&distance=5000&count=200&min_timestamp='+str(startTimestamp)+'&max_timestamp='+str(finishTimestamp))
+    photosJson = photosRequest.json()
+    for photoData in photosJson["data"]:
+        location = photoData["location"]
+        coordinate = Coordinate()
+        coordinate.latitude = location["latitude"]
+        coordinate.longitude = location["longitude"]
+
+        photo = Photo()
+        photo.coordinate = coordinate
+        photo.link = photoData["link"]
+
+        photos.append(photo)
+
+
+
+def findPhotos(coordinate):
+    DAYS_TO_RETRIVE = 7
+    #DAYS_TO_RETRIVE = 1
+    photos = []
+    currentTimestamp = int(time.time())
+    startTimestamp = currentTimestamp - 60*60*24*DAYS_TO_RETRIVE
+    sixHours = 60*60*6
+    finishTimestamp = startTimestamp + sixHours # Gets photos every 6 hours
+
+    threads = []
+    while finishTimestamp < currentTimestamp:
+        #print "LoopStart"
+
+        t = threading.Thread(target=makeRequest, args = (photos, coordinate, startTimestamp, finishTimestamp) )
+        threads.append(t)
+        #t.daemon = True
+        t.start()
+
+
+        startTimestamp += sixHours
+        finishTimestamp += sixHours
+
+        #print "LoopFinish"
+
+    for t in threads:
+        t.join()
+
+    if not photos: raise Exception("Empty photosCoordinates")
+
+    #for coord in photosCoordinates:
+
+    # Arreglar: Guardar objetos Photo en la lista, asi puedo hacer todo como un humano normal. Pasar quadtree a models.py
+    # Analizar: Poner de limite al Quadtree la minima y maxima latitud/longitud posible.
+
+    #print str(len(photosJson["data"])) + " ASD"
+    print "Cantidad de fotos: " + str(len(photos)) + "."
+
+    return photos
+
+
 #fixme: workaround
 @csrf_exempt
 def getCityInfo(request):
 
     if not request.POST["city-name"]:
-            return redirect('/')
+        return redirect('/')
 
     cityName = request.POST["city-name"]
-    r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+cityName)
-    cityJson = r.json()
+    mapRequest = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+cityName)
+    cityJson = mapRequest.json()
 
     if not cityJson["results"]:
         return redirect('/')
@@ -35,6 +98,46 @@ def getCityInfo(request):
     coordinate.latitude = cityJson["results"][0]["geometry"]["location"]["lat"]
     coordinate.longitude = cityJson["results"][0]["geometry"]["location"]["lng"]
 
+    photos = findPhotos(coordinate)
+    amountOfPhotos = len(photos)
+
+    totalDistance = 0
+    #photosWithDistances = [] #contains list [distance,photo]
+    for x in xrange(amountOfPhotos):
+        photo = photos[x]
+        #photosWithDistances.append([0,photo])
+
+        for y in xrange(x+1,amountOfPhotos):
+            distance = photo.distance(photos[y])
+            totalDistance += distance
+            #photosWithDistances[x][0] += distance
+            #photosWithDistances[y][0] += distance
+
+    averageDistance = (totalDistance*1.0) / amountOfPhotos
+    photosWithAmountOfCloseNeightbours = [] #(neightbours, photo)
+
+    for photo in photos:
+        amountOfPhotosNearerThanTheAverage = 0
+        for otherPhoto in photos:
+            if photo == otherPhoto: continue
+            if photo.distance(otherPhoto) < averageDistance:
+                amountOfPhotosNearerThanTheAverage += 1
+
+
+
+    #photosWithDistances.sort()
+
+    photoTuples = []
+    for photo in photos:
+        photoTuples.append( (photo.coordinate.latitude, photo.coordinate.longitude, photo.link) )
+
     #print request.POST
     #print coordinate.latitude
-    return render(request, 'index.html', {'coordinate':coordinate, 'cityName':cityName})
+    return render(
+        request, 'index.html',
+        {
+            'coordinate':coordinate,
+            'cityName':cityName,
+            'photoTuples': photoTuples
+        }
+    )
