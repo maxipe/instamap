1abf49c7f23bc19ec54a28c43e813e02208a19b4
maxipe <maxipenia@hotmail.com>
2016-01-26 04:10:22 +0100

    Finished. Extra: how not to use a CVS.

--- city/views.py
+++ city/views.py
@@ -11,15 +11,14 @@ import requests
 
 import time
 import threading
+import random
 
 import geopy.distance
 
 @csrf_exempt
 def index(request):
-    form = forms.CharField(label='City', max_length=100)
 
-    #return HttpResponse("Hello world! -")
-    return render(request, 'index.html', {'form':form})
+    return render(request, 'index.html', {})
 
 def findCoordinate(cityName):
     try:
@@ -53,37 +52,67 @@ def makeRequest(photos, coordinate, startTimestamp, finishTimestamp):
 
         photos.append(photo)
 
-def findPhotos(coordinate):
+def getCurrentDayInitTime(todayTime):
+    """ Returns the time of the start of
+    the day of the time recieved."""
+    string = str(todayTime.tm_year)
+    string += " "+str(todayTime.tm_mon)
+    string += " "+str(todayTime.tm_mday)
+    string += " 00:00:00"
+
+    return time.strptime(string, "%Y %m %d %H:%M:%S")
+
+def findPhotos(coordinate, initialHour=0, finalHour=24):
     """ Returns list of photos. """
     DAYS_TO_RETRIVE = 7
     #DAYS_TO_RETRIVE = 1
     photos = []
-    currentTimestamp = int(time.time())
-    startTimestamp = currentTimestamp - 60*60*24*DAYS_TO_RETRIVE
+    nowTimeStruct = time.localtime()
+    now = int(time.mktime(nowTimeStruct))
+
+    currentDayInitTime = getCurrentDayInitTime(nowTimeStruct)
+    currentDayInitTime = time.mktime(currentDayInitTime)
+
+    dayStart = currentDayInitTime - 60*60*24*DAYS_TO_RETRIVE + 60*60*initialHour
+    dayEnd = dayStart + 60*60*(finalHour-initialHour)
+
+    startTimestamp = dayStart
     sixHours = 60*60*6
-    finishTimestamp = startTimestamp + sixHours # Gets photos every 6 hours
+    finishTimestamp = startTimestamp
+
+    intervals = []
+
+    while startTimestamp < now:
+        finishTimestamp = startTimestamp + sixHours
+        if finishTimestamp > now:
+            finishTimestamp = now
+            intervals.append((startTimestamp, finishTimestamp))
+            break
+        elif finishTimestamp > dayEnd:
+            finishTimestamp = dayEnd
+            intervals.append((startTimestamp, finishTimestamp))
+            dayStart += 60*60*24
+            dayEnd = dayStart + 60*60*(finalHour-initialHour)
+            startTimestamp = dayStart
+            finishTimestamp = startTimestamp
+        else:
+            intervals.append((startTimestamp, finishTimestamp))
+            startTimestamp += sixHours
 
     threads = []
-    while finishTimestamp < currentTimestamp:
-        #print "LoopStart"
-
-        t = threading.Thread(target=makeRequest, args = (photos, coordinate, startTimestamp, finishTimestamp) )
+    for start, end in intervals:
+        #print time.ctime(start)
+        #print time.ctime(end)
+        t = threading.Thread(target=makeRequest, args = (photos, coordinate, start, end) )
         threads.append(t)
-        #t.daemon = True
         t.start()
-
-
-        startTimestamp += sixHours
-        finishTimestamp += sixHours
-
-        #print "LoopFinish"
 
     for t in threads:
         t.join()
 
     if not photos: raise Exception("Empty photosCoordinates")
 
-    print "Cantidad de fotos: " + str(len(photos)) + "."
+    #print "Photos #: " + str(len(photos)) + "."
 
     return photos
 
@@ -164,74 +193,57 @@ def comparePoints(p1,p2):
 #fixme: workaround
 @csrf_exempt
 def getCityInfo(request):
-
     if not request.POST["city-name"]:
         return redirect('/')
+
+    try:
+        if request.POST["initial-time"]:
+            initialHour = int(request.POST["initial-time"])
+        if request.POST["final-time"]:
+            finalHour = int(request.POST["final-time"])
+        if initialHour > finalHour or initialHour < 0 or finalHour > 24:
+            raise ValueError
+    except:
+        print "Invalid time."
+        initialHour = 0
+        finalHour = 24
+
+
 
     cityName = request.POST["city-name"]
     coordinate = findCoordinate(cityName)
     if not coordinate:
         return redirect('/')
 
-    photos = findPhotos(coordinate)
+    photos = findPhotos(coordinate, initialHour, finalHour)
 
     points = createPoints(coordinate)
-
 
     putPhotosInPoints(photos, points)
     points = sorted(points, cmp=comparePoints)
     photoTuples = []
-    """
-    print "A"
-    for x in xrange(10):
-        print len(points[x][1])
-    print "B"
-    for x in xrange(len(points)-10, len(points)):
-        print len(points[x][1])
-        #photoTuples.append( (points[x][0].latitude, points[x][0].longitude, "") )
-    """
+    photoSets = []
+    photoAlbums = [] # [ (#ofAlbum, [link1, link2]) ]
 
     for x in xrange(20):
-        for photo in points[x][1]:
-            photoTuples.append( (photo.coordinate.latitude, photo.coordinate.longitude, photo.link) )
+        try:
+            photoAlbums.append((x+1, []))
+            for photo in points[x][1]:
+                photoAlbums[x][1].append(photo.link)
+            photo = random.choice(points[x][1])
+            photoTuples.append( (photo.coordinate.latitude, photo.coordinate.longitude, "#"+str(x+1)) )
 
-    """
-    x = len(points)-1
-    photoTuples.append( (points[x][0].latitude, points[x][0].longitude, "CENTER") )
+        except:
+            break
 
-    z = 0
-    dict = {}
-    for photo in points[x][1]:
-        dict[str(points[x][0].latitude)+str(points[x][0].longitude)] = True
-        photoTuples.append( (photo.coordinate.latitude, photo.coordinate.longitude, str(z)) )
-        #print "Una foto"
-        print "a", str(points[x][0].latitude), str(points[x][0].longitude)
-        z += 1
-
-    """
-
-    """
-    photoTuples = []
-    for photo in photos:
-        photoTuples.append( (photo.coordinate.latitude, photo.coordinate.longitude, photo.link) )
-    """
-    """
-    photoTuples = []
-    for a in points:
-        coordi = a[0]
-        photoTuples.append( (coordi.latitude, coordi.longitude, "") )
-    """
-    #
-    #       PENSAR: Refinar haciendo menos Points, y despues dandole
-    #       bola a donde haya mas photos.
-
-    #print request.POST
-    #print coordinate.latitude
     return render(
         request, 'index.html',
         {
             'coordinate':coordinate,
             'cityName':cityName,
-            'photoTuples': photoTuples
+            'photoTuples': photoTuples,
+            'photoAlbums': photoAlbums,
+            'initialHour': initialHour,
+            'finalHour': finalHour
         }
     )
